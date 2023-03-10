2b58510075b806009a9b2f670920f8fde14f824c
Adrian B <adrian.biedny@student.hogent.be>
2023-03-10 20:24:54 +0100

    Add: DomainException | Implement(+Refactor):GasCard(TankCard)

---
+++ BusinessLayer/Exceptions/DomainException.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessLayer.Exceptions;
+
+internal class DomainException : Exception
+{
+    internal DomainException(string message) : base(message) { }
+    internal DomainException(string message, Exception inner) : base(message, inner) { }
+}
---
+++ BusinessLayer/Model/GasCard.cs
@@ -0,0 +1,66 @@
+﻿using BusinessLayer.Exceptions;
+using static BussinessLayer.Model.Vehicle;
+
+namespace BussinessLayer.Model;
+
+/*
+ * Adrian B on 10/03
+ */
+
+public class GasCard
+{
+    //attrib
+    private string _cardNumber;
+    private DateTime _expiringDate;
+    private int? _pincode;
+
+    //ctor
+    public GasCard(string cardNumber, DateTime expiringDate, int? pincode, bool blocked, List<FuelType> fuel)
+    {
+        CardNumber = cardNumber;
+        ExpiringDate = expiringDate;
+        Pincode = pincode;
+        Blocked = blocked;
+        Fuel = fuel;
+    }
+
+    //prop
+    public string CardNumber
+    {
+        /*
+         * must be unique: unique constraint in DB, no need to keep local list to make sure for unique val
+         * DB will throw unique constraint error if double data entry, caught my Repo/RepoException
+         */
+        get { return _cardNumber; }
+        set
+        {
+            if (value == null) throw new DomainException("GasCard: Set-CardNumber: NULL val");
+            _cardNumber = value;
+        }
+    }
+    public DateTime ExpiringDate
+    {
+        get { return _expiringDate; }
+        set
+        {
+            //DateTime cannot be null => default val for DateTime means data entry was NULL
+            if (value == default(DateTime)) throw new DomainException("GasCard: Set-DateTime: NULL val");
+            _expiringDate = value;
+        }
+    }
+    public int? Pincode
+    {
+        get { return _pincode; }
+        set
+        {
+            if (value != null)
+            {
+                if (0 > value || 9999 < value) throw new DomainException("GasCard: Set-Pincode: Pin lower than 0 or higher than 9999");
+                _pincode = value;
+            }
+            _pincode = value;
+        }
+    }
+    public bool Blocked { get; set; }
+    public List<FuelType> Fuel { get; set; }
+}
