0867b547397184454edec4f2839e966b384ce411
merlin-8 <merlin.ngalula@student.hogent.be>
2023-03-16 10:03:06 +0100

    update driverspage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,28 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import DriverPage from './pages/Driver';
-import GascardPage from './pages/Gascard';
-import RootLayout from './pages/Root';
-import StartScreen from './pages/Start';
-import VehiclePage from './pages/Vehicle';
+import DriverPage from "./pages/Driver";
+import GascardPage from "./pages/Gascard";
+import RootLayout from "./pages/Root";
+import StartScreen from "./pages/Start";
+import VehiclePage from "./pages/Vehicle";
 
 // Router
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
         index: true,
         element: <StartScreen />,
       },
-      { path: 'vehicles', element: <VehiclePage /> },
-      { path: 'drivers', element: <DriverPage /> },
-      { path: 'gascards', element: <GascardPage /> },
+      { path: "vehicles", element: <VehiclePage /> },
+      { path: "drivers", element: <DriverPage /> },
+      { path: "gascards", element: <GascardPage /> },
     ],
   },
 ]);
-const App = props => {
+const App = (props) => {
   return <RouterProvider router={router} />;
 };
 
--- frontend/src/pages/Driver.jsx
+++ frontend/src/pages/Driver.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../css/Driver.css";
+import logo from "../images/logoLong.png";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { BsFillPencilFill } from "react-icons/bs";
+import { TiArrowRightThick } from "react-icons/ti";
 
 const DriverPage = () => {
-  return <h1>Drivers</h1>;
+  const [drivers, setDrivers] = useState([]);
+
+  const fetchData = () => {
+    axios.get("https://swapi.dev/api/people").then((response) => {
+      setDrivers(response.data.results);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>Bestuurders</h1>
+        <img src={logo} alt="logo"></img>
+      </div>
+
+      <hr className="line" />
+
+      <div className="one">
+        <input type="text" id="searchTxtbox" name="searchTxtBox"></input>
+        <button type="button">Zoek</button>
+        <button type="button">Toevoegen</button>
+      </div>
+
+      <div className="two">
+        <>
+          {drivers &&
+            drivers.length > 0 &&
+            drivers.map((drivers) => (
+              <li key={drivers.id}>
+                <p>{drivers.name} </p>
+                <p className="icons">
+                  <RiDeleteBin5Line />
+                  <BsFillPencilFill />
+                  <TiArrowRightThick />
+                </p>
+              </li>
+            ))}
+        </>
+      </div>
+    </div>
+  );
 };
 
 export default DriverPage;
