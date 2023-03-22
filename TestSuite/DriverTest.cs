25751a0dbe895767f10ca310a38dbe1dda2665a8
Dietbrand <dietbrand.tollenaere@gmail.com>
2023-03-22 16:45:44 +0100

    Added more tests for driver

--- TestSuite/DriverTest.cs
+++ TestSuite/DriverTest.cs
@@ -1,57 +1,86 @@
-﻿using BusinessLayer.Model;
-using BusinessLayer.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessLayer.Exceptions;
+using BusinessLayer.Model;
 
-namespace TestSuite {
-    public class DriverTest {
-        [Fact]
-        public void TestDriver_NullValuesForFields_ThrowExcepion() {
-            Assert.Throws<DomainException>(() => new Driver(1, null, null, null, null));
-        }
+namespace TestSuite;
+public class DriverTest {
 
-        [Theory]
-        [InlineData(0)]
-        [InlineData(-1)]
-        [InlineData(int.MinValue)]
-        public void TestDriver_InvalidId_ThrowException(int id) {
-            List<DriversLicense> licenses = new List<DriversLicense>();
-            licenses.Add(DriversLicense.B);
-            Assert.Throws<DomainException>(() => new Driver(id, "voornaam", "achternaam", "75.10.23-059.39", licenses));
-            //Driver d = new Driver(1, "voornaam", "achternaam", geboortedatum, "75.10.23-059.39", licenses);
-            //Assert.Equal(1, d.Id);
-        }
+    [Fact]
+    public void TestDriver_ValidData_ValidCtor() {
+        // Arrange
+        string firstName = "Morgan";
+        string lastName = "Freeman";
+        string rrn = "85.10.23-059.39";
+        List<DriversLicense> licenses = new() {
+            DriversLicense.B
+        };
+        Driver d = new(1, lastName, firstName, rrn, licenses);
 
-        [Theory]
-        [InlineData("")]
-        [InlineData(null)]
-        public void TestDriver_InvalidFirstName_ThrowException(string firstname) {
-            List<DriversLicense> licenses = new List<DriversLicense>();
-            licenses.Add(DriversLicense.B);
-            Assert.Throws<DomainException>(() => new Driver(1, firstname, "achternaam", "75.10.23-059.39", licenses));
-        }
+        Assert.Equal(1, d.Id);
+        Assert.Equal(rrn, d.NatRegNumber);
+        Assert.Equal(firstName, d.FirstName);
+        Assert.Equal(lastName, d.LastName);
+        Assert.Equal(licenses, d.Licenses);
+    }
 
-        //TODO: Extract birthdate from RRN then this test can be refactored to check if Birthdate == birthdate in RRN
-        /* [Fact]
-        public void TestDriver_InvalidBirthDateToRRN_ThrowException() {
-            DateTime geboortedatum = new DateTime(1976, 10, 23);
-            List<DriversLicense> licenses = new List<DriversLicense>();
-            licenses.Add(DriversLicense.B);
-            Assert.Throws<DomainException>(() => new Driver(1, "voornaam", "achternaam", geboortedatum, "75.10.23-059.39", licenses));
-        }
-        */
+    [Fact]
+    public void TestDriver_ValidData_BirthdatefromRRN() {
+        // Arrange
+        string firstName = "Bobby";
+        string lastName = "Tables";
+        string rrn = "85.10.23-059.39";
+        List<DriversLicense> licenses = new() {
+            DriversLicense.B
+        };
+        DateTime geboortedatum = new DateTime(1985, 10, 23);
+        Driver d = new(1, lastName, firstName, rrn, licenses);
 
-        [Theory]
-        [InlineData(null)]
-        // [InlineData("0.0.0-000.00")] // TODO: this should not work
-        // [InlineData("00.00.00-000.00")] // TODO: this should not work
-        public void TestDriver_InvalidRRN_ThrowException(string rrn) {
-            List<DriversLicense> licenses = new List<DriversLicense>();
-            licenses.Add(DriversLicense.B);
-            Assert.Throws<DomainException>(() => new Driver(1, "voornaam", "achternaam", rrn, licenses));
-        }
+        Assert.Equal(geboortedatum, d.BirthDate);
+    }
+
+    [Fact]
+    public void TestDriver_NullValuesForFields_ThrowExcepion() {
+        Assert.Throws<DomainException>(() => new Driver(1, null, null, null, null));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(int.MinValue)]
+    public void TestDriver_InvalidId_ThrowException(int id) {
+        List<DriversLicense> licenses = new List<DriversLicense>();
+        licenses.Add(DriversLicense.B);
+        Assert.Throws<DomainException>(() => new Driver(id, "voornaam", "achternaam", "75.10.23-059.39", licenses));
+    }
+
+    [Theory]
+    [InlineData("", "")]
+    [InlineData(null, null)]
+    [InlineData("    ", "Bobby")]
+    [InlineData("Mallory", "   ")]
+    [InlineData("Bob", "\t")]
+    [InlineData("Alice", "\n")]
+    [InlineData("Alice", null)]
+    public void TestDriver_InvalidNames_ThrowException(string firstname, string lastname) {
+        List<DriversLicense> licenses = new List<DriversLicense>();
+        licenses.Add(DriversLicense.B);
+        Assert.Throws<DomainException>(() => new Driver(1, firstname, lastname, "75.10.23-059.39", licenses));
+    }
+
+    [Fact]
+    public void TestDriver_InvalidBirthDateToRRN_ThrowException() {
+        DateTime geboortedatum = new DateTime(1976, 10, 23);
+        List<DriversLicense> licenses = new List<DriversLicense>();
+        licenses.Add(DriversLicense.B);
+        Assert.Throws<DomainException>(() => new Driver(1, "voornaam", "achternaam", geboortedatum, "75.10.23-059.39", licenses));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("0.0.0-000.00")]
+    // [InlineData("00.00.00-000.00")] // TODO: this should not work
+    public void TestDriver_InvalidRRN_ThrowException(string rrn) {
+        List<DriversLicense> licenses = new List<DriversLicense>();
+        licenses.Add(DriversLicense.B);
+        Assert.Throws<DomainException>(() => new Driver(1, "voornaam", "achternaam", rrn, licenses));
     }
 }
