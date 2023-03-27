6a50f884a6d8230451d4a01b6ec13db2defc772a
Adrian B <adrian.biedny@student.hogent.be>
2023-03-27 20:11:22 +0200

    Created GasCardInfo & VehicleInfo DTO's

---
+++ BusinessLayer/DTO/GasCardInfo.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTO;
+
+public class GasCardInfo
+{
+	public GasCardInfo(GasCard gc, Driver? d)
+	{
+		GasCardID = gc.Id;
+		CardNumber = gc.CardNumber;
+		ExpiringDate = gc.ExpiringDate;
+		Pincode = gc.Pincode;
+		FuelTypes = gc.Fuel;
+		Blocked = gc.Blocked;
+
+        if (d != null) Driver = d;
+    }
+
+	public int? GasCardID { get; set; }
+	public string CardNumber { get; set; }
+	public DateTime ExpiringDate { get; set; }
+	public int? Pincode { get; set; }
+	public List<FuelType>? FuelTypes { get; set; }
+	public bool Blocked { get; set; }
+	public Driver? Driver { get; set; }
+}
---
+++ BusinessLayer/DTO/VehicleInfo.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTO;
+
+public class VehicleInfo
+{
+	public VehicleInfo(Vehicle v, Driver? d)
+	{
+		VIN = v.VinNumber;
+		BrandModel = v.BrandModel;
+		FuelType = v.Fuel;
+		VehicleType = v.Category;
+		Color = v.Color;
+		Doors = v.Doors;
+        if (d != null) Driver = d;
+    }
+
+	public string VIN { get; set; }
+	public string BrandModel { get; set; }
+	public string LicensePlate { get; set; }
+	public FuelType FuelType { get; set; }
+	public VehicleType VehicleType { get; set; }
+	public string? Color { get; set; }
+	public int? Doors { get; set; }
+	public Driver? Driver { get; set; }
+}
