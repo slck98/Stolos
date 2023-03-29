96c2d925457c216a55829a50bc3354dd9a1784e4
Selçuk Özer <selcuk.ozer.y7657@student.hogent.be>
2023-03-29 23:10:15 +0200

    update driver page

--- frontend/src/components/DriverEdit.jsx
+++ frontend/src/components/DriverEdit.jsx
@@ -1,9 +1,72 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import foto from '../images/user.png';
+import classes from '../css/DriverEdit.module.css';
 
 const DriverEdit = ({ driver }) => {
   const [input, setInput] = useState();
+  const navigate = useNavigate();
 
-  return <article>{driver.firstName}</article>;
+  const changeHandler = e => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <article className={classes.container}>
+      <img src={foto} alt="notAvailable" />
+      <article className={classes.one}>
+        <span>
+          <p>Naam:</p>
+          <input
+            type="text"
+            name="firstName"
+            defaultValue={driver.firstName}
+            onChange={changeHandler}
+          />
+        </span>
+        <span>
+          <p>Familienaam:</p>
+          <input
+            type="text"
+            name="lastName"
+            defaultValue={driver.lastName}
+            onChange={changeHandler}
+          />
+        </span>
+        <span>
+          <p>Geboortedatum:</p>
+          <input
+            type="text"
+            name="birthDate"
+            defaultValue={driver.birthDate}
+            onChange={changeHandler}
+          />
+        </span>
+        <span>
+          <p>Rijksregisternummer:</p>
+          <input
+            type="text"
+            name="natRegNumber"
+            defaultValue={driver.natRegNum}
+            onChange={changeHandler}
+          />
+        </span>
+        <span>
+          <p>Adres:</p>
+          <input
+            type="text"
+            name="address"
+            defaultValue={driver.address}
+            onChange={changeHandler}
+          />
+        </span>        
+      </article>
+      <article className={classes.two}>
+        <p onClick={() => navigate(-1)}>Opslaan</p>
+        <p onClick={() => navigate(-1)}>Annuleren</p>
+      </article>
+    </article>
+  );
 };
 
 export default DriverEdit;
--- frontend/src/components/DriverItem.jsx
+++ frontend/src/components/DriverItem.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import classes from '../css/DriverItem.module.css';
+import foto from '../images/user.png';
+import DeletePopup from './DeletePopup';
 
 const DriverItem = ({ driver }) => {
-  return <div>{driver.firstName}</div>;
+  const [deletePopup, setDeletePopup] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <article className={classes.container}>
+        <article className={classes.back}>
+          <p onClick={() => navigate(-1)}>Terug</p>
+        </article>
+        <img src={foto} alt="User" />
+        <article className={classes.one}>
+          <h4>{driver.firstName} {driver.lastName}</h4>
+          <p>Geboortedatum: {driver.birthDate}</p>
+          <p>Rijksregisternummer: {driver.natRegNum}</p>
+          <p>Adres: {driver.address}</p>
+          <p>License: {driver.licenses}</p>
+        </article>
+
+        <article className={classes.two}>
+          <p onClick={() => navigate(`/drivers/${driver.driverID}/edit`)}>
+            Bewerken
+          </p>
+          <p onClick={() => setDeletePopup(true)}>Verwijderen</p>
+        </article>
+
+        {/* <article className={classes.three}>
+          <h3>Alle Voertuigen</h3>
+
+          <ul>
+            <li></li>
+          </ul>
+        </article> */}
+      </article>
+
+      <DeletePopup trigger={deletePopup} setTrigger={setDeletePopup}>
+        <p>Zeker dat u deze bestuurder wilt verwijderen?</p>
+      </DeletePopup>
+    </>
+  );
 };
 
 export default DriverItem;
