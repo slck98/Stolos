70cdff499a9bfa7d4b6fd5ddfa82d5c136c9282f
Dietbrand <dietbrand.tollenaere@gmail.com>
2023-04-20 12:43:04 +0200

    HTML table semantics and cardnumber instead of id

--- frontend/src/components/DriverEdit.jsx
+++ frontend/src/components/DriverEdit.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import foto from "../images/user.png";
-import classes from "../css/DriverEdit.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBan, faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import foto from '../images/user.png';
+import classes from '../css/DriverEdit.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBan, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 
 const DriverEdit = ({ driver }) => {
   const [input, setInput] = useState();
   const navigate = useNavigate();
 
-  const changeHandler = (e) => {
+  const changeHandler = e => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
 
@@ -17,61 +17,63 @@ const DriverEdit = ({ driver }) => {
     <article className={classes.container}>
       <img src={foto} alt="notAvailable" />
       <table className={classes.one}>
-        <tr>
-          <td>Naam:</td>
-          <td>
-            <input
-              type="text"
-              name="firstName"
-              defaultValue={driver.firstName}
-              onChange={changeHandler}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>Familienaam:</td>
-          <td>
-            <input
-              type="text"
-              name="lastName"
-              defaultValue={driver.lastName}
-              onChange={changeHandler}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>Geboortedatum:</td>
-          <td>
-            <input
-              type="text"
-              name="birthDate"
-              defaultValue={driver.birthDate}
-              onChange={changeHandler}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>Rijksregisternummer:</td>
-          <td>
-            <input
-              type="text"
-              name="natRegNumber"
-              defaultValue={driver.natRegNum}
-              onChange={changeHandler}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td>Adres:</td>
-          <td>
-            <input
-              type="text"
-              name="address"
-              defaultValue={driver.address}
-              onChange={changeHandler}
-            />
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Naam:</td>
+            <td>
+              <input
+                type="text"
+                name="firstName"
+                defaultValue={driver.firstName}
+                onChange={changeHandler}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Familienaam:</td>
+            <td>
+              <input
+                type="text"
+                name="lastName"
+                defaultValue={driver.lastName}
+                onChange={changeHandler}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Geboortedatum:</td>
+            <td>
+              <input
+                type="text"
+                name="birthDate"
+                defaultValue={driver.birthDate}
+                onChange={changeHandler}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Rijksregisternummer:</td>
+            <td>
+              <input
+                type="text"
+                name="natRegNumber"
+                defaultValue={driver.natRegNum}
+                onChange={changeHandler}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Adres:</td>
+            <td>
+              <input
+                type="text"
+                name="address"
+                defaultValue={driver.address}
+                onChange={changeHandler}
+              />
+            </td>
+          </tr>
+        </tbody>
       </table>
       <article className={classes.button}>
         <p onClick={() => navigate(-1)} className={classes.opslaan}>
--- frontend/src/components/Lists.jsx
+++ frontend/src/components/Lists.jsx
@@ -2,29 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import vehicleClasses from '../css/VehicleList.module.css';
 import driverClasses from '../css/DriverList.module.css';
-import gasCardClasses from "../css/GascardList.module.css";
+import gasCardClasses from '../css/GascardList.module.css';
 import logo from '../images/logoLong.png';
 
 const DriverList = ({ drivers }) => {
-    return (
-      <header className={driverClasses.container}>
-        <article className={driverClasses.headerPage}>
-          <h1>Bestuurders</h1>
-          <img src={logo} alt="logo"></img>
-        </article>
-  
-        <hr className={driverClasses.line} />
-        <ul>
-          {drivers.map(driver => (
-            <li key={driver.driverID.toString()}>
-              {console.log(driver)}
-              <Link to={driver.driverID.toString()}>{driver.firstName}</Link>
-            </li>
-          ))}
-        </ul>
-      </header>
-    );
-  };
+  return (
+    <header className={driverClasses.container}>
+      <article className={driverClasses.headerPage}>
+        <h1>Bestuurders</h1>
+        <img src={logo} alt="logo"></img>
+      </article>
+
+      <hr className={driverClasses.line} />
+      <ul>
+        {drivers.map(driver => (
+          <li key={driver.driverID.toString()}>
+            <Link to={driver.driverID.toString()}>{driver.firstName}</Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
 
 export const VehicleList = ({ vehicles }) => {
   return (
@@ -46,24 +45,23 @@ export const VehicleList = ({ vehicles }) => {
   );
 };
 
-
 export const GascardList = ({ gascards }) => {
-    return (
-      <header className={gasCardClasses.container}>
-        <article className={gasCardClasses.headerPage}>
-          <h1>Brandstofkaarten</h1>
-          <img src={logo} alt="logo" />
-        </article>
-  
-        <hr className={gasCardClasses.line} />
-        <ul>
-          {gascards.map((gascard) => (
-            <li key={gascard.gasCardID.toString()}>
-              <Link to={gascard.gasCardID.toString()}>{gascard.cardNumber}</Link>
-            </li>
-          ))}
-        </ul>
-      </header>
-    );
-  };
+  return (
+    <header className={gasCardClasses.container}>
+      <article className={gasCardClasses.headerPage}>
+        <h1>Brandstofkaarten</h1>
+        <img src={logo} alt="logo" />
+      </article>
+
+      <hr className={gasCardClasses.line} />
+      <ul>
+        {gascards.map(gascard => (
+          <li key={gascard.cardNumber.toString()}>
+            <Link to={gascard.cardNumber.toString()}>{gascard.cardNumber}</Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
 export default DriverList;
